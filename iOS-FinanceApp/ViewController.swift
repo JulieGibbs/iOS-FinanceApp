f309cd9dbeab745127686352efd01041d2c5e857
JulieGibbs <harrisonbeckham1106@gmail.com>
2020-03-16 00:00:18 +0300

    Initial Commit

---
+++ iOS-FinanceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-FinanceApp
+//
+//  Created by Dmitry Aksyonov on 16.03.2020.
+//  Copyright © 2020 Dmitry Aksyonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

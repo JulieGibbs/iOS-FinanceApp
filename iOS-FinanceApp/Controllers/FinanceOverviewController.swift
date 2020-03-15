a3aed57219c9daf5d38a58ca80047417e5abfa07
JulieGibbs <harrisonbeckham1106@gmail.com>
2020-03-16 00:38:11 +0300

    Added App's View blueprint and viewController dummies

---
+++ iOS-FinanceApp/Controllers/FinanceOverviewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS-FinanceApp
+//
+//  Created by Dmitry Aksyonov on 16.03.2020.
+//  Copyright © 2020 Dmitry Aksyonov. All rights reserved.
+//
+
+import UIKit
+
+class FinanceOverviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

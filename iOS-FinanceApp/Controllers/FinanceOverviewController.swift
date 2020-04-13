6577b13272a72e9f6596300099e569057b2b2243
JulieGibbs <harrisonbeckham1106@gmail.com>
2020-04-14 01:09:00 +0800

    Work on TableView
    Added default realm notification mechanism + still dealing with nils..

--- iOS-FinanceApp/Controllers/FinanceOverviewController.swift
+++ iOS-FinanceApp/Controllers/FinanceOverviewController.swift
@@ -14,11 +14,10 @@ class FinanceOverviewController: UIViewController {
     @IBOutlet weak var financeOverviewTableView: UITableView!
     @IBOutlet weak var currentBalanceLabel: UILabel!
     
-    //    var tableEntries: Results<Entry>!
     var realm = try! Realm()
     let tableEntries = try! Realm().objects(Entry.self)
     let entriesManager = EntriesManager()
-    
+    var notificationToken: NotificationToken?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,20 +26,37 @@ class FinanceOverviewController: UIViewController {
         self.financeOverviewTableView.reloadData()
         financeOverviewTableView.delegate = self
         financeOverviewTableView.dataSource = self
+        
+        self.notificationToken = tableEntries.observe({ (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial:
+                self.financeOverviewTableView.reloadData()
+            case .update (_, let deletions, let insertions, let modifications):
+                self.financeOverviewTableView.beginUpdates()
+                self.financeOverviewTableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                self.financeOverviewTableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                self.financeOverviewTableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                self.financeOverviewTableView.endUpdates()
+            case .error(let err):
+                fatalError("\(err)")
+            }
+        })
     }
     
     @IBAction func addIncome(_ sender: UIButton) {
         let entry = Entry()
-        var entryTag = "A"
+        let entryTag = "A"
         entry.amount = Int.random(in: 1000...5000)
         entry.name = entryTag
         writeToRealm(write: entry)
-        entryTag += "'!"
-        financeOverviewTableView.reloadData()
     }
     
     @IBAction func addExpense(_ sender: UIButton) {
-        
+        let entry = Entry()
+        let entryTag = "B"
+        entry.amount = Int.random(in: 1000...5000) * -1
+        entry.name = entryTag
+        writeToRealm(write: entry)
     }
     
     private func writeToRealm(write item: Entry) {
@@ -50,13 +66,13 @@ class FinanceOverviewController: UIViewController {
     }
 }
 
-extension FinanceOverviewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        return UISwipeActionsConfiguration()
-    }
-    
+extension FinanceOverviewController: UITableViewDelegate {    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
+        if editingStyle == .delete {
+            realm.beginWrite()
+            realm.delete(tableEntries[indexPath.row])
+            try! realm.commitWrite()
+        }
     }
 }
 

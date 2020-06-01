cd479d53c9ec0367f78eb13e3444f7920ba88016
JulieGibbs <harrisonbeckham1106@gmail.com>
2020-06-01 04:54:20 +0300

    technical commit

--- iOS-FinanceApp/Controllers/GraphViewController.swift
+++ iOS-FinanceApp/Controllers/GraphViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 import RealmSwift
 
 class GraphViewController: UIViewController {
+    let segmentedControl = UISegmentedControl()
+    let lineGraphView = LineGraphView()
+    let pieChartView = PieChartView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func setupSegmentedControl() {
+        segmentedControl.insertSegment(withTitle: "Day", at: 0, animated: true)
+        segmentedControl.insertSegment(withTitle: "Week", at: 1, animated: true)
+        segmentedControl.insertSegment(withTitle: "Month", at: 2, animated: true)
+        segmentedControl.insertSegment(withTitle: "Quarter", at: 3, animated: true)
+        segmentedControl.insertSegment(withTitle: "Year", at: 4, animated: true)
+    }
+    func setupLineGraphView() {}
+    func setupPieChartView() {}
 }
